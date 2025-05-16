c33fee1437763954f72e5c78ce0c30491ee2fbfc
melijimenez11 <melijimenez@berkeley.edu>
2025-05-16 22:23:15 +0100

    parallax ready

--- app/components/Home/HeroMain.tsx
+++ app/components/Home/HeroMain.tsx
@@ -1,7 +1,7 @@
 'use client'
 import * as React from 'react'
 import Image from 'next/image'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useCallback } from 'react'
 
 import Typography from '@mui/material/Typography'
 
@@ -13,30 +13,56 @@ import mouse from '@/public/img/homepage-hero/mouse.png'
 import { ParallaxBanner, ParallaxBannerLayer } from 'react-scroll-parallax'
 
 function HeroMain() {
+    const introTextRef = useRef<HTMLDivElement | null>(null)
+    const secTextRef = useRef<HTMLDivElement | null>(null)
 
+    const handleScroll = useCallback((e: Event): void => {
+        const introNode = introTextRef.current
+        if (!introNode) return
+
+        const secNode = secTextRef.current
+        if (!secNode) return
+
+        const scrollY = window.scrollY
+
+        if (scrollY > 300) {
+            introNode.classList.add('hidden')
+            secNode.classList.add('visible')
+        } else {
+            introNode.classList.remove('hidden')
+            secNode.classList.remove('visible')
+        }
+    }, [])
+
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll, { passive: true })
+
+        return () => window.removeEventListener('scroll', handleScroll)
+    }, [handleScroll])
     return (
         <div className="homepage-hero">
             <ParallaxBanner style={{ height: '100vh' }}>
-                <ParallaxBannerLayer speed={-25} expanded={false}>
+                <ParallaxBannerLayer speed={-20} >
                     <img src={sky.src} className="layer joshua-hero" alt="sky hero" />
                 </ParallaxBannerLayer>
-                <ParallaxBannerLayer expanded={false} speed={10}>
+                <ParallaxBannerLayer expanded={false} speed={35}>
                     <div className="rocks-container">
                         <img src={rocks.src} className="layer rocks" alt="joshua tree hero" />
                     </div>
                 </ParallaxBannerLayer>
                 <ParallaxBannerLayer >
                     <div className="layer text">
-                        <div className="intro">
+                        <div ref={introTextRef} className="intro">
                             <Typography className="intro__title" variant="h2">Explore Next-Gen Climate Data</Typography>
                             <Typography className="intro__p">Cal-Adapt delivers critical climate data and cutting-edge tools to empower communities, researchers, and decision-makers to take action now. As climate impacts intensify, we provide the insights needed to adapt, build resilience, and drive urgent solutions for a sustainable future.</Typography>
                             <div className="intro__scroll">
                                 <Typography variant="caption">Scroll</Typography>
                                 <Image src={mouse} className="mouse" alt="mouse symbol guiding the user to scroll down" />
                             </div>
-                            <div className="secondary">
-                                <Typography className="secondary__title" variant="h2">Data Driven Tools for a Resilient Future</Typography>
-                            </div>
+                        </div>
+                        <div ref={secTextRef} className="secondary hidden">
+                            <Typography className="secondary__title" variant="h2">Data Driven Tools for a Resilient Future</Typography>
                         </div>
                     </div>
 
