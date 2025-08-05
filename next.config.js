089657ae2e2de49b7e6e5405ecc51d53c37dcccb
melijimenez11 <melijimenez@berkeley.edu>
2025-08-05 12:37:06 -0600

    aws build & deployment preparation

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
-
-module.exports = {
-    // ...
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-  };
+module.exports = nextConfig;
